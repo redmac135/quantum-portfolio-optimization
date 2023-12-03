369b81148b8820dcd0362db39fcf424233426a20
redmac135 <ethanzhao1981@gmail.com>
2023-12-03 17:54:13 -0500

    add options endpoint

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+STOCK_OPTIONS = ("apple", "bob miller")
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/options")
+async def listOptions():
+    return {"stocks": STOCK_OPTIONS}
