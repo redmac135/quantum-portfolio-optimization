723db66cfaee1d979a2c16e9a8dc25fcb11905ef
Akshat Khanna <akhann48@uwo.ca>
2023-12-31 15:35:08 -0500

    Akshats tasks (had some problems, especially with the second task)

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,12 @@
-# import requests
-# from settings import API_URL
-
+import requests
+from settings import API_URL
 
 def get_stock_data(ticker: str) -> dict:
     # this will just return the raw data from the api
-    ...
+    # Put your generated API key in the url below
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey='
+    r = requests.get(url)
+    data = r.json
+    print(data)
+    return data
     raise NotImplementedError
