bd9f21140ae629882ad3d1e14a568e26be0b4f67
Hartej <hrandh9@uwo.ca>
2024-01-04 15:41:12 -0500

    adding function to calculate covariance

--- backend/utils.py
+++ backend/utils.py
@@ -8,10 +8,23 @@ def get_historical_stock_prices(ticker: str) -> list:
     raise NotImplementedError
 
 
-def calculate_covarience(list1: list, list2: list) -> float:
+def calculate_covariance(list1: list, list2: list) -> float:
     if not len(list1) == len(list2):
         raise ValueError("Lists must be the same length")
+    
+    n = len(list1)
 
-    # should return the covarience of the two lists
-    ...
-    raise NotImplementedError
+    # Calculate the mean of each list
+    mean_list1 = sum(list1) / n
+    mean_list2 = sum(list2) / n
+
+    # Initialize the covariance sum
+    cov_sum = 0.0
+
+    # Calculate the covariance
+    for i in range(n):
+       cov_sum += (list1[i] - mean_list1) * (list2[i] - mean_list2)
+    
+    covariance = cov_sum / (n - 1)
+    
+    return covariance
