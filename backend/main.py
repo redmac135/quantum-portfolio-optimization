dc19079f202d32b4836e9d6031094bf2ecc802b4
redmac135 <ethanzhao1981@gmail.com>
2024-02-04 13:00:05 -0500

    format data for wqc model

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from settings import STOCK_OPTIONS
+from settings import STOCK_DATA
 from typing import Dict
 from utils import get_historical_stock_prices, calculate_covariance
 import itertools
@@ -15,7 +15,7 @@ class PredictionRequest(BaseModel):
 
 @app.get("/options")
 async def listOptions() -> Dict[str, list[str]]:
-    return {"stocks": STOCK_OPTIONS}
+    return {"stocks": list(STOCK_DATA.keys())}
 
 
 @app.post("/predict")
@@ -23,7 +23,7 @@ async def predict(request: PredictionRequest):
     # list stock data
     stock_data: Dict[str, float] = {}
     for stock in request.stock_choices:
-        if stock not in STOCK_OPTIONS:
+        if not stock in STOCK_DATA.keys():
             raise ValueError(f"Stock {stock} not in options")
         stock_data[stock] = get_historical_stock_prices(stock)
 
@@ -35,5 +35,12 @@ async def predict(request: PredictionRequest):
         covarience_matrix[pair] = calculate_covariance(
             stock_data[pair[0]], stock_data[pair[1]]
         )
+        if not pair[0] == pair[1]:
+            covarience_matrix[(pair[1], pair[0])] = covarience_matrix[pair]
+
+    # data to send to quantum model
+    covarience_matrix
+    returns_dict = {stock: STOCK_DATA[stock][0] for stock in request.stock_choices}
+    esg_dict = {stock: STOCK_DATA[stock][1] for stock in request.stock_choices}
 
     raise NotImplementedError
--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,14 @@
 # contains backend settings and constants
 
 # temporary
-STOCK_OPTIONS = ("apple", "bob miller")
+STOCK_DATA = {
+    'AAPL': (0.1, 0.1),
+    'GOOGL': (0.15, 0.15),
+    'MSFT': (0.22, 0.2),
+    'AMZN': (0.25, 0.25),
+    'FB': (0.3, 0.3),
+    'TSLA': (0.35, 0.35),
+    'NVDA': (0.4, 0.4),
+    'INTC': (0.45, 0.45),
+    'CSCO': (0.5, 0.5),
+}
