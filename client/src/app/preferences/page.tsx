c98d00c111f42db52692e3daa00a45d1354b4038
JakeNizio <jake.nizio2@gmail.com>
2024-03-22 13:25:15 -0400

    fixed slider array error, implemented mobile responsiveness

--- client/src/app/preferences/page.tsx
+++ client/src/app/preferences/page.tsx
@@ -25,6 +25,15 @@ export default function Preferences() {
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     setIsLoading(true);
+    
+    const result : Submission = {
+        risk: e.currentTarget.risk.value,
+        esg: e.currentTarget.esg.value,
+        principle: Number(e.currentTarget.principle.value)
+    }
+
+    console.log(result);
+
 
     // try {
     //   const formData = new FormData(e.currentTarget);
@@ -41,18 +50,31 @@ export default function Preferences() {
   return (
     <div id={styles["page"]}> 
         <div id={styles["alignment"]}>
+            <div id={styles["background-image"]}>
+                <Image
+                    src="/preferencesBackground.png"
+                    priority={true}
+                    width={546}
+                    height={832}
+                    alt="background-image"
+                />
+            </div>
             <div id={styles["container"]}>
                 <div className={styles["bubble"]}>
                     <h1>Portfolio Preference Dashboard</h1>
                 </div>
-                {isLoading ? <h1>Loading...</h1> : 
+                {isLoading ? 
+                <div className={styles["loading"]}>
+                    <h1>Loading...</h1>
+                </div>
+                : 
                 <form className={styles["form"]} onSubmit={handleSubmit}>
                     <div className={styles["input"]}>
                         <h2>Risk Tolerance</h2>
                         <div className={styles["slider"]}>
                             <input type="range" name="risk" min={1} max={RISK_MAX} value={risk} onChange={(e) => setRisk(Number(e.target.value))}/>
                             <div className={styles["slider-numbers"]}>
-                                {[...Array(RISK_MAX).keys()].map((val) => (
+                                {Array.from(Array(RISK_MAX).keys()).map((val) => (
                                     <p key={val} className={(risk === val + 1) ? styles["slider-number-focus"] : styles["slider-number-blur"]}>{val + 1}</p>
                                 ))}
                             </div>
@@ -67,7 +89,7 @@ export default function Preferences() {
                         <div className={styles["slider"]}>
                             <input type="range" name="esg" min={1} max={ESG_MAX} value={esg} onChange={(e) => setEsg(Number(e.target.value))}/>
                             <div className={styles["slider-numbers"]}>
-                                {[...Array(ESG_MAX).keys()].map((val) => (
+                                {Array.from(Array(ESG_MAX).keys()).map((val) => (
                                     <p key={val} className={(esg === val + 1) ? styles["slider-number-focus"] : styles["slider-number-blur"]}>{val + 1}</p>
                                 ))}
                             </div>
@@ -78,7 +100,7 @@ export default function Preferences() {
                         </div>
                     </div>
                     <div className={styles["input"]}>
-                        <h2>Principal Investment</h2>
+                        <h2>Principle Investment</h2>
                         <div className={styles["field"]}>
                             <p>$</p>
                             <input type="number" name="principle" required={true} placeholder={"69"} step={0.01}/>
