a53651734acb53998306351712bd234640a86f35
Ethan Zhao <ethanzhao1981@gmail.com>
2024-07-13 15:41:07 -0400

    completely redo frontend

---
+++ client/config/site.ts
@@ -0,0 +1,23 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Quantum Portfolio Optimization",
+  description:
+    "Combining detailed qualitative analysis with quantum computing to minimize risk and maximize returns.",
+  navItems: [
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  links: {
+    github:
+      "https://github.com/redmac135/quantum-portfolio-optimization/tree/dev",
+  },
+};
